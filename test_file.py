3a6b15df52bdbd6750aeeb0b87cc2e917cd39af0
Ben Love <benuklove@users.noreply.github.com>
2016-06-16 01:30:02 -0500

    update

--- test_file.py
+++ test_file.py
@@ -1,6 +1,25 @@
-# print("Hello PyCharm!")
+# -*- coding: utf-8 -*-
+from selenium import selenium
+import unittest, time, re
 
-people = ['Jonas', 'Julio', 'Mike', 'Mez']
-ages = [25, 30, 31, 39]
-for person, age in zip(people, ages):
-    print(person, age)
+
+class Loves(unittest.TestCase):
+    def setUp(self):
+        self.verificationErrors = []
+        self.selenium = selenium("localhost", 4444, "*chrome", "https://www.loves.com/")
+        self.selenium.start()
+
+    def test_loves(self):
+        sel = self.selenium
+        sel.open("/en/location-and-fuel-price-search/locationsearchresults#?state=All")
+        sel.click("//div[@id='welcomePopup']/ul/li[2]/a/div")
+        sel.click("link=Export")
+        sel.wait_for_page_to_load("30000")
+
+    def tearDown(self):
+        self.selenium.stop()
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
