5c35bbf8e3c46745e985c23a6844e7933f1d7995
Ben Love <benuklove@users.noreply.github.com>
2016-06-18 21:07:14 -0500

    Scrapy
    brief scrapy tutorial stuff

---
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,21 @@
+"""
+Created on 6/18/2016 by Ben
+
+First spider for scrapy tutorial
+"""
+
+import scrapy
+
+
+class DmozSpider(scrapy.Spider):
+    name = "dmoz"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
